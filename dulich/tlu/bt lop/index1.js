cc73381b7c3e0b3b63ce4fa79e8cb67b177a8787
Tien1398 <Tien.vitconxauxi@gmail.com>
2018-12-16 23:56:37 +0700

    btlon

--- dulich/tlu/bt lop/index1.js
+++
@@ -1,8 +0,0 @@
-
-$(document).ready(function(){
-  $('#showFuit').click(function(){
-      $('#showpic').html(
-         showpic.innerHTML='<img src="banana.jpg" alt="Smiley face" height="100" width="100">')
-  })
-}
-)
--- dulich/tlu/dulich/public/js/btlon.js
+++
@@ -1,8 +0,0 @@
- // lấy phương thức
-var modal = document.getElementById('id01');       
-// Khi người dùng nhấp vào bất cứ nơi nào bên ngoài phương thức, hãy đóng nó lại
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
